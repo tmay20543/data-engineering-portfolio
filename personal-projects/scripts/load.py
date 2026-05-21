8d7d4f9fa102197fe2c8933305a9f6c186712065
tmay20543 <tmay20543@gmail.com>
2026-05-21 20:09:17 +0200

    Restructuring the code into extract,transform and load modules. Incomplete.

--- personal-projects/scripts/load.py
+++ personal-projects/scripts/load.py
@@ -1,4 +1,8 @@
+import pandas as pd
 
-clean_df.to_csv("countries_clean.csv", index=False)
+def load_data(transformed_data):
+    df.to_csv("countries.csv", index=False)
+    df.to_json("countries.json")
+    clean_df.to_csv("countries_clean.csv", index=False)
 
-print(df.shape)
+    print(df.shape)
